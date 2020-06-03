34a2602f5720d10938aa1ddcd821dc255b881675
ishaqmusaq <58223968+ishaqmusaq@users.noreply.github.com>
2020-06-03 16:12:14 +0100

    first commit

---
+++ js/style.js
@@ -0,0 +1,36 @@
+document.getElementById('grayButton').onclick = switchGray;
+document.getElementById('whiteButton').onclick = switchWhite;
+document.getElementById('blueButton').onclick = switchBlue;
+document.getElementById('yellowButton').onclick = switchYellow;
+document.getElementById('redButton').onclick = switchRed;
+document.getElementById('violetButton').onclick = switchViolet;
+
+function switchGray() {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'gray'; 
+  document.getElementsByTagName('body')[0].style.color = 'white'; 
+}
+
+function switchWhite() {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'white'; 
+  document.getElementsByTagName('body')[0].style.color = 'black'; 
+}
+
+function switchBlue() {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'blue'; 
+  document.getElementsByTagName('body')[0].style.color = 'white'; 
+}
+
+function switchYellow() {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'yellow'; 
+  document.getElementsByTagName('body')[0].style.color = 'black'; 
+}
+
+function switchRed() {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'red'; 
+  document.getElementsByTagName('body')[0].style.color = 'white'; 
+}
+
+function switchViolet() {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'violet'; 
+  document.getElementsByTagName('body')[0].style.color = 'pink'; 
+}
